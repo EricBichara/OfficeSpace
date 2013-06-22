412b0bd11810c1172972d9e786639235a27afb24
ericbichara <eric.bichara@gmail.com>
2013-06-22 14:26:58 +0200

    refactoring

---
+++ client/controllers/AdminController.js
@@ -0,0 +1,13 @@
+app.controllerProvider.register('AdminController', function ($scope, officeService) {
+    $scope.name = "";
+    $scope.type = "";
+
+    $scope.fetchOffices = function(){
+        var data = officeService.fetchOffices().then(function(data){
+            $scope.name = data.name;
+            $scope.type = data.type;
+        });
+
+    }
+
+});
