776d64795e4a0627e75aef523094ea4f350ed7c0
ericbichara <eric.bichara@gmail.com>
2013-06-22 14:53:08 +0200

    refactoring

--- client/services/OfficeService.js
+++ client/services/OfficeService.js
@@ -1,7 +1,7 @@
 app.service('officeService', function ($http) {
     this.fetchOffices = function () {
         return $http.get('/getOffices').then(function(result){
-               return result.data;
+            return result.data;
         });
     }
 });
